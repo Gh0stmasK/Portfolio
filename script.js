0bbac8049662c334941a8db71e85220e00d7bd02
Gh0stmasK <soura20v25@gmail.com>
2025-12-15 16:48:38 +0530

    Changes to be committed: 	modified:   script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 document.getElementById('mobile-menu').onclick = function () {
     document.querySelector('.mobile-menu-overlay').classList.add('active');
+    document.querySelector('header').style.backdropFilter = 'none';
+    document.querySelector('body,html').style.overflow = 'hidden';
 }
 
 document.getElementById('close-btn').onclick = function () {
     document.querySelector('.mobile-menu-overlay').classList.remove('active');
+    document.querySelector('header').style.backdropFilter = 'blur(30px)';
+    document.querySelector('body,html').style.overflow = 'auto';
 }
