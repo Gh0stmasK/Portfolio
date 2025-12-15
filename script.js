6746d69112e04605f6ae00df89d4484a235020c4
Gh0stmasK <soura20v25@gmail.com>
2025-12-15 23:02:23 +0530

    modified html  css and js

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ window.onload = function () {
     footerElement.innerHTML = `<p>&copy; ${new Date().getFullYear()} <u>Designed</u> and <u>Coded</u> with ❤️ by Sourav Chakraborty</p>`;
     document.body.appendChild(footerElement);
 }
+
+const copyButton1 = document.getElementById('copy-btn-1');
+const textToCopy1 = document.getElementById('email');
+
+copyButton1.addEventListener('click', () => {
+    const contentToCopy = textToCopy1.textContent || textToCopy1.innerText;
+    navigator.clipboard.writeText(contentToCopy)
+        .then(() => {
+            console.log('Text successfully copied to clipboard');
+            alert('Email copied!');
+        })
+        .catch(err => {
+            console.error('Unable to copy text: ', err);
+            alert('Failed to copy text. Please copy manually.');
+        });
+});
+
+const copyButton2 = document.getElementById('copy-btn-2');
+const textToCopy2 = document.getElementById('phone');
+
+copyButton2.addEventListener('click', () => {
+    const contentToCopy = textToCopy2.textContent || textToCopy2.innerText;
+    navigator.clipboard.writeText(contentToCopy)
+        .then(() => {
+            console.log('Text successfully copied to clipboard');
+            alert('Phone number copied!');
+        })
+        .catch(err => {
+            console.error('Unable to copy text: ', err);
+            alert('Failed to copy text. Please copy manually.');
+        });
+});
