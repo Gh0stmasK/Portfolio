c76202affd93a583ba9cc08b166f86350c1553f0
Gh0stmasK <soura20v25@gmail.com>
2025-12-15 18:45:55 +0530

    Changes to be committed: 	modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ document.getElementById('close-btn').onclick = function () {
     document.querySelector('header').style.backdropFilter = 'blur(30px)';
     document.querySelector('body,html').style.overflow = 'auto';
 }
+
+window.onload = function () {
+    const footerElement = document.createElement("footer");
+    footerElement.innerHTML = `<p>&copy; ${new Date().getFullYear()} <u>Designed</u> and <u>Coded</u> with ❤️ by Sourav Chakraborty</p>`;
+    document.body.appendChild(footerElement);
+}
